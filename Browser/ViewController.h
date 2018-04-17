8d67725fee92dd956ec92aa2d15a706b72b11f60
Rolando Cruz <rolandocruz@rolandos-mbp.citrite.net>
2018-04-17 15:24:19 -0400

    Initial Commit

---
+++ Browser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Browser
+//
+//  Created by Rolando Cruz on 4/17/18.
+//  Copyright © 2018 Citrix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Browser/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Browser
+//
+//  Created by Rolando Cruz on 4/17/18.
+//  Copyright © 2018 Citrix. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
